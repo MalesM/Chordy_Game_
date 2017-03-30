03709b521412c1220c98a87835b877c73c38978b
MalesM <markommales@gmail.com>
2017-03-30 20:08:45 +0200

    pred zavrsnicu

--- app/src/main/java/com/example/gospodin/chordy_game/GameActivity.java
+++ app/src/main/java/com/example/gospodin/chordy_game/GameActivity.java
@@ -1,10 +1,13 @@
 package com.example.gospodin.chordy_game;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -12,8 +15,10 @@ import java.util.Random;
 public class GameActivity extends AppCompatActivity {
 
     Button b1, b2, b3, b4, playB;
-    private int flag1=0, flag2;
+    private int flagPlay=0, flagAnswer=0, flagPick = 0;
     private int answerID;
+    private int score, count;
+    private Drawable d;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,31 +27,31 @@ public class GameActivity extends AppCompatActivity {
 
         inic();
         changeAnswers();
+        d = b1.getBackground();
     }
 
     public void checkAnswer(View v){
-        if (flag1 == 1){
+        if (flagPlay == 1){
             switch (v.getId()){
-                case R.id.b1: if (b1.getText().toString().equals(MainActivity.chordNames[answerID])){
-                    processPositive(); b1.setBackgroundColor(Color.GREEN);}
-                    else {processNegative(); b1.setBackgroundColor(Color.RED);}
+                case R.id.b1: if (b1.getText().toString().equals(MainActivity.chordNames[answerID]) && flagPick == 0){
+                    processPositive(); b1.setBackgroundColor(Color.GREEN); flagAnswer=1; flagPick=1;}
+                    else {processNegative(); b1.setBackgroundColor(Color.RED); flagAnswer=1; flagPick=1;}
                     break;
-                case R.id.b2: if (b2.getText().toString().equals(MainActivity.chordNames[answerID])){
-                    processPositive(); b2.setBackgroundColor(Color.GREEN);}
-                else {processNegative(); b2.setBackgroundColor(Color.RED);}
+                case R.id.b2: if (b2.getText().toString().equals(MainActivity.chordNames[answerID]) && flagPick == 0){
+                    processPositive(); b2.setBackgroundColor(Color.GREEN); flagAnswer=1; flagPick=1;}
+                    else {processNegative(); b2.setBackgroundColor(Color.RED); flagAnswer=1; flagPick=1;}
                     break;
-                case R.id.b3: if (b3.getText().toString().equals(MainActivity.chordNames[answerID])){
-                    processPositive(); b3.setBackgroundColor(Color.GREEN);}
-                else {processNegative(); b3.setBackgroundColor(Color.RED);}
+                    case R.id.b3: if (b3.getText().toString().equals(MainActivity.chordNames[answerID]) && flagPick == 0){
+                    processPositive(); b3.setBackgroundColor(Color.GREEN); flagAnswer=1; flagPick=1;}
+                    else {processNegative(); b3.setBackgroundColor(Color.RED); flagAnswer=1; flagPick=1;}
                     break;
-                case R.id.b4: if (b4.getText().toString().equals(MainActivity.chordNames[answerID])){
-                    processPositive(); b4.setBackgroundColor(Color.GREEN);}
-                else {processNegative(); b4.setBackgroundColor(Color.RED);}
+                    case R.id.b4: if (b4.getText().toString().equals(MainActivity.chordNames[answerID]) && flagPick == 0){
+                    processPositive(); b4.setBackgroundColor(Color.GREEN); flagAnswer=1; flagPick=1;}
+                    else {processNegative(); b4.setBackgroundColor(Color.RED); flagAnswer=1; flagPick=1;}
                     break;
-                default: throw new RuntimeException("Unknown button ID");
-
+                    default: throw new RuntimeException("Unknown button ID");
             }
-        }
+        }else {Toast toast = Toast.makeText(this, "Listen first!",Toast.LENGTH_SHORT);toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0, 0) ;toast.show();}
     }
 
     public void changeAnswers(){
@@ -63,7 +68,7 @@ public class GameActivity extends AppCompatActivity {
     public void play(View v){
         if (MainActivity.loaded) {
             MainActivity.soundPool.play(MainActivity.soundID[answerID], 1, 1, 1, 0, 1f);
-            flag1 = 1;
+            flagPlay = 1;
         }
     }
 
@@ -73,6 +78,19 @@ public class GameActivity extends AppCompatActivity {
 
     public void processNegative(){
 
+    }
+
+    public void next(View v){
+        if (flagAnswer == 0 ){
+            Toast toast = Toast.makeText(this, "Choose answer!",Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0, 0) ;
+            toast.show();
+        }else{
+            changeAnswers();
+            resetButtonColor();
+            flagAnswer = 0;
+            flagPlay = 0;
+        }
     }
 
     private class Answer{
@@ -105,6 +123,12 @@ public class GameActivity extends AppCompatActivity {
         return new Answer(niz2, element);
     }
 
+    public void resetButtonColor(){
+        b1.setBackground(d);
+        b2.setBackground(d);
+        b3.setBackground(d);
+        b4.setBackground(d);
+    }
     public void inic(){
         b1 = (Button) findViewById(R.id.b1);
         b2 = (Button) findViewById(R.id.b2);
