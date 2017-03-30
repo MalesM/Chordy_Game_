0ed6ae2c5cde0f7466bbd2782914e4b7ddd22bf2
MalesM <markommales@gmail.com>
2017-03-30 15:34:28 +0200

    dodata prva provera resenja

--- app/src/main/java/com/example/gospodin/chordy_game/GameActivity.java
+++ app/src/main/java/com/example/gospodin/chordy_game/GameActivity.java
@@ -1,5 +1,6 @@
 package com.example.gospodin.chordy_game;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,7 +12,7 @@ import java.util.Random;
 public class GameActivity extends AppCompatActivity {
 
     Button b1, b2, b3, b4, playB;
-    private int flag1, flag2;
+    private int flag1=0, flag2;
     private int answerID;
 
     @Override
@@ -24,14 +25,55 @@ public class GameActivity extends AppCompatActivity {
     }
 
     public void checkAnswer(View v){
+        if (flag1 == 1){
+            switch (v.getId()){
+                case R.id.b1: if (b1.getText().toString().equals(MainActivity.chordNames[answerID])){
+                    processPositive(); b1.setBackgroundColor(Color.GREEN);}
+                    else {processNegative(); b1.setBackgroundColor(Color.RED);}
+                    break;
+                case R.id.b2: if (b2.getText().toString().equals(MainActivity.chordNames[answerID])){
+                    processPositive(); b2.setBackgroundColor(Color.GREEN);}
+                else {processNegative(); b2.setBackgroundColor(Color.RED);}
+                    break;
+                case R.id.b3: if (b3.getText().toString().equals(MainActivity.chordNames[answerID])){
+                    processPositive(); b3.setBackgroundColor(Color.GREEN);}
+                else {processNegative(); b3.setBackgroundColor(Color.RED);}
+                    break;
+                case R.id.b4: if (b4.getText().toString().equals(MainActivity.chordNames[answerID])){
+                    processPositive(); b4.setBackgroundColor(Color.GREEN);}
+                else {processNegative(); b4.setBackgroundColor(Color.RED);}
+                    break;
+                default: throw new RuntimeException("Unknown button ID");
 
+            }
+        }
     }
 
     public void changeAnswers(){
         Answer answer = myArray(MainActivity.chordNames);
+        b1.setText(answer.getArr()[0]);
+        b2.setText(answer.getArr()[1]);
+        b3.setText(answer.getArr()[2]);
+        b4.setText(answer.getArr()[3]);
+
+        answerID = answer.getAnswer();
 
     }
 
+    public void play(View v){
+        if (MainActivity.loaded) {
+            MainActivity.soundPool.play(MainActivity.soundID[answerID], 1, 1, 1, 0, 1f);
+            flag1 = 1;
+        }
+    }
+
+    public void processPositive(){
+
+    }
+
+    public void processNegative(){
+
+    }
 
     private class Answer{
         private String[] arr;
